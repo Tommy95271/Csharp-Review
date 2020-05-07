be2f3ef2e425743b19107282cf5a820e436eb901
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-07 09:36:51 +0800

    Initialize Csharp solution.

---
+++ CsharpDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine($"Args {i} : {args[i]}");
+            }
+
+            string[] strArr = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", strArr));
+
+            Console.ReadLine();
+        }
+    }
+}
