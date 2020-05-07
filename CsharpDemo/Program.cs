f7228cba1f3775a99063d9eb02e63957d4f05b41
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-07 10:14:10 +0800

    Add DateTime, TimeSpan, Number and formats.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +11,38 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine($"Args {i} : {args[i]}");
-            }
+            #region DateTime and TimeSpan
+            DateTime dateTime = new DateTime(1989, 6, 4);
+            Console.WriteLine($"old date: {dateTime}");
 
-            string[] strArr = Environment.GetCommandLineArgs();
-            Console.WriteLine(string.Join(", ", strArr));
+            dateTime = dateTime.AddDays(7);
+            dateTime = dateTime.AddMonths(3);
+            dateTime = dateTime.AddYears(2);
+            Console.WriteLine($"new date: {dateTime}");
 
-            Console.ReadLine();
+            TimeSpan workingTime = new TimeSpan(9, 0, 0);
+            Console.WriteLine($"Working time: {workingTime}");
+
+            workingTime = workingTime.Subtract(new TimeSpan(1, 0, 0));
+            workingTime = workingTime.Add(new TimeSpan(0, 30, 0));
+            Console.WriteLine($"Working time: {workingTime}");
+            #endregion
+
+            #region Number types
+            BigInteger bigNum = BigInteger.Parse("1234512345123451234512345");
+            Console.WriteLine($"bigNum * 2 : {bigNum * 2}");
+            #endregion
+
+            #region Format
+            Console.WriteLine($"Currency : {23.5:c}");
+            Console.WriteLine($"Pads with 0: {23:d4}");
+            Console.WriteLine($"3 Decimals : {23:f3}");
+            Console.WriteLine($"Commas : {2300000:n1}");
+
+            #endregion
+
+
+
         }
     }
 }
