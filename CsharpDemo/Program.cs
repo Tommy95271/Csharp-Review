eb6f2cc7ada46343d4f2dedc9d3613db743ac9f4
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-08 11:57:31 +0800

    Params keyword, same concept as Rest Operator in Javascript.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -28,15 +28,23 @@ namespace CsharpDemo
             #endregion
 
             #region Swap
-            int num1 = 10;
-            int num2 = 20;
-            Console.WriteLine($"Before swap, num1 = {num1}, num2 = {num2}");
-            Swap(ref num1, ref num2); // There is another way to change data inside other functions, use 'ref' param.
-            Console.WriteLine($"After swap, num1 = {num1}, num2 = {num2}");
-            // The difference between 'ref' and 'out' is small, since they are all 'Pass by reference', which means you
-            // can not use both of them to make overload functions. The key difference is that you need to initialize the variable
-            // using 'ref' before manipulating it. However, it's opposite for 'out' param, as long as you assign value to 'out'
-            // variable in the end of function, then it'll be ok.'
+            //int num1 = 10;
+            //int num2 = 20;
+            //Console.WriteLine($"Before swap, num1 = {num1}, num2 = {num2}");
+            //Swap(ref num1, ref num2); // There is another way to change data inside other functions, use 'ref' param.
+            //Console.WriteLine($"After swap, num1 = {num1}, num2 = {num2}");
+            //// The difference between 'ref' and 'out' is small, since they are all 'Pass by reference', which means you
+            //// can not use both of them to make overload functions. The key difference is that you need to initialize the variable
+            //// using 'ref' before manipulating it. However, it's opposite for 'out' param, as long as you assign value to 'out'
+            //// variable in the end of function, then it'll be ok.'
+            #endregion
+
+            #region Param
+            // There is a easy way to get all arguments if you don't want know how many arguments you will have.
+            // Use 'params' keyword before parameter inside function.'
+            // If you've learned Javascript before, then you should be familiar with it, in Javascript it's called 'Rest Operator'.
+            // Check this website https://stackoverflow.com/questions/39886700/javascript-spread-syntax-in-c-sharp
+            Console.WriteLine($"1 + 2 + 3 = {GetSum(1, 2, 3)}");
             #endregion
         }
 
@@ -63,6 +71,17 @@ namespace CsharpDemo
             int temp = num1;
             num1 = num2;
             num2 = temp;
+        }
+        #endregion
+        #region Param
+        static double GetSum(params double[] nums)
+        {
+            double sum = 0;
+            foreach (var n in nums)
+            {
+                sum += n;
+            }
+            return sum;
         }
         #endregion
         #endregion
