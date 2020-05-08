34880594ce348962f5d36273e9f1e38f32cda075
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-08 11:00:32 +0800

    Clear.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -12,16 +12,7 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            StringBuilder sb = new StringBuilder("This is a sentence");
-            StringBuilder sb2 = new StringBuilder("This is a more important sentence", 256);
-            Console.WriteLine($"Capacity : {sb.Capacity}\nCapacity : {sb2.Capacity}");
-            Console.WriteLine($"Length : {sb.Length}");
 
-            sb2.AppendLine("\nMore important sentence");
-            CultureInfo enUS = CultureInfo.CreateSpecificCulture("en-US");
-            string goodMan = "Tommy";
-            sb2.AppendFormat(enUS, $"Good Man {goodMan}");
-            Console.WriteLine(sb2.ToString());
 
         }
     }
