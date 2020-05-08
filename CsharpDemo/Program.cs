ff9622fd13322d1084b34956fc968e09a014f026
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-08 11:19:57 +0800

    It is no normal way to get data from other functions.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -12,12 +12,32 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            double x = 10;
-            double y = 4;
-            Console.WriteLine($"x + y = {GetDouble(x,y)}");
-            Console.WriteLine($"x is still {x}, y is still {y}");
+            #region GetDouble
+            //double x = 10;
+            //double y = 4;
+            //Console.WriteLine($"x + y = {GetDouble(x, y)}");
+            //Console.WriteLine($"x is still {x}, y is still {y}");
+            #endregion
 
+            #region DoubleIt
+
+            double num = 15; //As you can see, we assign number to num and times except solution.
+            double times = 2;
+            double solution; //We want to get solution by DoubleIt function, but as the example before,
+            DoubleIt(num, times, solution); //We can not get data from another function.
+            Console.WriteLine($"{num} * {times} = {solution}");
+            #endregion
         }
+
+
+        #region helper
+        #region DoubleIt
+        private static void DoubleIt(double dou, double times, double solution)
+        {
+            solution = dou * times;
+        }
+        #endregion
+        #region GetDouble
         private static double GetDouble(double x=1,double y = 1)
         {
             double temp = x;
@@ -25,5 +45,9 @@ namespace CsharpDemo
             y = temp;
             return x + y;
         }
+        #endregion
+
+        #endregion
+
     }
 }
