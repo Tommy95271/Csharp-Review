52e929ab665eb6c694ff50b8ccb64692c93450f2
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-09 15:32:53 +0800

    Add Animal class in prop way, you can see how easy it is.

--- CsharpDemo/Animal.cs
+++ CsharpDemo/Animal.cs
@@ -8,6 +8,46 @@ namespace CsharpDemo
 {
     public class Animal
     {
+        private string name;
+        private string sound;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.Any(char.IsDigit))
+                {
+                    value = "No Name";
+                }
+                name = value;
+            }
+        }
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if (value.Length > 10)
+                {
+                    value = "No Sound";
+                }
+                sound = value;
+            }
+        }
 
+        public void MakeSound()
+        {
+            Console.WriteLine($"{Name} says {Sound}");
+        }
+
+        public Animal()
+            : this("No Name", "No Sound") { }
+        public Animal(string name)
+            : this(name, "No Sound") { }
+        public Animal(string name, string sound)
+        {
+            Name = name;
+            Sound = sound;
+        }
     }
 }
