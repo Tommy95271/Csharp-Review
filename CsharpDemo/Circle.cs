7e95c6845d2d6cf18a383b2671c472d5bf0fb458
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-11 15:47:15 +0800

    Adds prop, constructor and override methods in Circle.

---
+++ CsharpDemo/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; } = 0;
+        public Circle(double radius)
+        {
+            Name = "Circle";
+            Radius = radius;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"This is a Circle");
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
