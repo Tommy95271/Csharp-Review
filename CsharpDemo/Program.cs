77539334d0cf71f8b14e5b1832a392f32b688e34
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-11 15:16:58 +0800

    .

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -15,6 +15,10 @@ namespace CsharpDemo
             Warrior Bob = new Warrior("Bob",1000,120,34);
             Warrior Ted = new Warrior("Ted",1000,120,43);
             Battle.StartFight(Bob, Ted);
+
+            //Warrior Luke = new Warrior("Luke", 1200, 50, 12);
+            //Warrior Oui = new Warrior("Oui", 1200, 40, 30);
+            //Battle.StartFight(Luke, Oui);
         }
     }
 }
