44d13169055eb633f3c10cbb9a9b3073ce44e894
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-11 15:43:38 +0800

    Add prop and methods inside Shape, you can see virtual and abstract are different.

--- CsharpDemo/Shape.cs
+++ CsharpDemo/Shape.cs
@@ -8,5 +8,11 @@ namespace CsharpDemo
 {
     abstract class Shape
     {
+        public string Name { get; set; } = "No Name";
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+        public abstract double Area();
     }
 }
