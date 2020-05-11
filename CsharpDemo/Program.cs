2675852df21e9bd87447343f0c7d5e33005bf0b5
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-11 21:04:10 +0800

    Clear.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -12,26 +12,7 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            Shape[] shapes = { new Circle(4), new Rectangle(5, 7) };
-            foreach (Shape s in shapes)
-            {
-                Console.WriteLine($"The Area of {s.Name} is {s.Area():f2}");
-                s.GetInfo();
-                Circle testCric = s as Circle;
-                if (testCric == null)
-                {
-                    Console.WriteLine("This is not a Circle");
-                }
-                if (s is Circle)
-                {
-                    Console.WriteLine("This is not a Rectangle\n");
-                }
-            }
-            Console.WriteLine();
-            object cir1 = new Circle(5);
-            Circle cir2 = (Circle)cir1;
-            Console.WriteLine($"{cir2.Name} Area : {cir2.Area():f2}");
-            Console.WriteLine(cir2.GetType());
+
         }
     }
 }
