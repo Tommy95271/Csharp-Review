454ff437f80cb0247a6a950489578f17342cda24
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-12 21:05:39 +0800

    Third, we define a interface ICommand which contains the methods of functionality.

--- CsharpDemo/ICommand.cs
+++ CsharpDemo/ICommand.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace CsharpDemo
 {
+    // Third, we define a interface ICommand which contains the methods of functionality.
     interface ICommand
     {
         void Execute();
