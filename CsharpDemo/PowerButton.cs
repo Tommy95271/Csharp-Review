5197f6326071ed1cf20a7642a78bc5105995b0aa
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-12 21:14:29 +0800

    Fourth, we define a class PowerButton and inherit from ICommand, this step is important, we create a device of IElectronicDevice but we do not instantiate it, in the constructor, we connect the parameter and the prop, if we pass any argument with IElectronicDevice interface, we can use the methods Execute (which executes device.On()) and Undo (which executes device.Off()), but we can not instantiate interface, what should we do?

--- CsharpDemo/PowerButton.cs
+++ CsharpDemo/PowerButton.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace CsharpDemo
 {
+    // Fourth, we define a class PowerButton and inherit from ICommand, this step is important,
+    // we create a device of IElectronicDevice but we do not instantiate it, in the constructor,
+    // we connect the parameter and the prop, if we pass any argument with IElectronicDevice interface,
+    // we can use the methods Execute (which executes device.On()) and Undo (which executes device.Off()),
+    // but we can not instantiate interface, what should we do?
     class PowerButton : ICommand
     {
         IElectronicDevice device;
