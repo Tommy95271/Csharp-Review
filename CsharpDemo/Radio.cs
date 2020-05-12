b957da2b5a3b7c7cd93d112ffdb7800593c981b7
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-12 08:22:01 +0800

    Adds Radio class and inherit from IElectronicDevice interface.

---
+++ CsharpDemo/Radio.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpDemo
+{
+    class Radio : IElectronicDevice
+    {
+        public void Off()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void On()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VolumnDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VolumnUp()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
