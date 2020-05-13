d3e3663bc0c079405b1c36e6a6862ff997ee5361
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-13 10:43:26 +0800

    Defines 6 basic props.

--- CsharpDemo/Furniture.cs
+++ CsharpDemo/Furniture.cs
@@ -8,5 +8,12 @@ namespace CsharpDemo
 {
     abstract class Furniture
     {
+        public int Years { get; set; } = 1990;
+        public int Legs { get; set; } = 0;
+        public string Color { get; set; } = "No Color";
+        public double Weight { get; set; } = 0;
+        public double Length { get; set; } = 0;
+        public double Price { get; set; } = 0;
+
     }
 }
