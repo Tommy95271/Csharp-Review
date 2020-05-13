b59b572168109fa468711481161440e4d39b0769
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-13 10:24:17 +0800

    Clear.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -10,15 +10,7 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < args.Length; i++)
-            {
-                Console.WriteLine($"Args {i} : {args[i]}");
-            }
 
-            string[] strArr = Environment.GetCommandLineArgs();
-            Console.WriteLine(string.Join(", ", strArr));
-
-            Console.ReadLine();
         }
     }
 }
