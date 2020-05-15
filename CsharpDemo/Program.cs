d5e50ddebe68dcfd031cfaa1446c833a4fd99d20
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-15 20:42:26 +0800

    Clear.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -12,33 +12,7 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            Car car = new Car("Ford", 4, 160);
-            if (car is IDrivable)
-            {
-                car.Drive();
-                car.Stop();
-            }
-            else
-            {
-                Console.WriteLine($"{car.Brand} can't be driven.");
-            }
 
-            // Since interfaces can not not be instantiated, so we need a static method to return a new instance.
-            // Six, delcare a TV with IElectronicDevice and assign TVRemote.GetDevice() to it, which achieve instantiating new Television.
-            IElectronicDevice TV = Remote.GetDevice("TV");
-            // Seventh, instantiate a PowerButton named TVPowBtn, and pass TV into it.
-            PowerButton TVPowBtn = new PowerButton(TV);
-            // Eighth, we can execute Execute() and Undo() now.
-            TVPowBtn.Execute();
-            TVPowBtn.Undo();
-            // Ninth, see the type of TV, it is Television.
-            Console.WriteLine(TV.GetType());
-
-            IElectronicDevice Radio = Remote.GetDevice("Radio");
-            PowerButton RadioPowBtn = new PowerButton(Radio);
-            RadioPowBtn.Execute();
-            RadioPowBtn.Undo();
-            Console.WriteLine(Radio.GetType());
         }
     }
 }
