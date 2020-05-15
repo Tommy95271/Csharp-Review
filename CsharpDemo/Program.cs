a28b914794b3a1c6c3633349efe30d30def7a9ec
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-15 21:06:01 +0800

    Add ArrayList, remember using System.Collections, and you can see Count means number, Capacity means the capacity that computer assigns to the program, Capacity is always greater than or equal to Count. Capacity starts with 4, then 8, 16, 32...

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -5,6 +5,7 @@ using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using System.Collections;
 
 namespace CsharpDemo
 {
@@ -12,7 +13,21 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
+            #region ArrayList
+            ArrayList aList = new ArrayList();
+            aList.Add("Tommy");
+            aList.Add(50);
 
+            Console.WriteLine($"Number of aList : {aList.Count}");
+            Console.WriteLine($"Capacity of aList : {aList.Capacity}");
+
+            aList.Add(false);
+            aList.Add("Bob");
+            aList.Add(true);
+
+            Console.WriteLine($"Number of aList : {aList.Count}");
+            Console.WriteLine($"Capacity of aList : {aList.Capacity}");
+            #endregion
         }
     }
 }
