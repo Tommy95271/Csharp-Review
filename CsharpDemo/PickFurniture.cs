6eacce29dff7543a9e2df52af757c14a78dda15e
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-15 16:47:11 +0800

    Ok, we pass arguments into GetFurniture method and pass them into Bed and Chari.

--- CsharpDemo/PickFurniture.cs
+++ CsharpDemo/PickFurniture.cs
@@ -6,17 +6,24 @@ using System.Threading.Tasks;
 
 namespace CsharpDemo
 {
-    public class PickFurniture
+    public class PickFurniture : Furniture
     {
-        public static Furniture GetFurniture(MyEnum myEnum)
+        public static Furniture GetFurniture(MyEnum myEnum,
+            string name = "No Name",
+            int years = 0,
+            int legs = 0,
+            string color = "No Color",
+            double weight = 0,
+            double length = 0,
+            double price = 0)
         {
             if (myEnum == MyEnum.Bed)
             {
-                return new Bed();
+                return new Bed(name, years, legs, color, weight, length, price);
             }
             else if (myEnum == MyEnum.Chair)
             {
-                return new Chair();
+                return new Chair(name, years, legs, color, weight, length, price);
             }
             else
             {
