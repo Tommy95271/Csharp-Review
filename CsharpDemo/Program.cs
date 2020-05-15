8c0b8b09def1ec30377300c9d64598ee8f22cf38
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-15 19:46:24 +0800

    As you can see, we can rewrite cleanTools as chair in parameter,

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -14,14 +14,20 @@ namespace CsharpDemo
             //bed.GetInfo();
             //bed.Wash();
 
+            Console.WriteLine("-------------------------------");
             Furniture bed2 = PickFurniture.GetFurniture(MyEnum.Bed);
             //bed2.GetInfo();
-
-            Console.WriteLine("-------------------------------");
 
             CleanTools cleanTools = new CleanTools(bed2, true, "Good Bed", 1989, 4, "Red", 250, 64, 1989);
             cleanTools.Clean();
             cleanTools.GetInfo();
+
+            Console.WriteLine("-------------------------------");
+            Furniture chair = PickFurniture.GetFurniture(MyEnum.Chair);
+            cleanTools = new CleanTools(chair, false, "Blood chair", 1991, 6, "Yellow", 64, 64, 999);
+            cleanTools.Clean();
+            cleanTools.GetInfo();
+            Console.ReadLine();
         }
     }
 }
