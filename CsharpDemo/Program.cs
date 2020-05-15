163c5ddc1e1945539867818042fc1cbf6579b58f
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-15 19:21:35 +0800

    Something goes wrong, looks like bed2 and cleanTools are not connected, you can see other parameters in cleanTools are all default values.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -10,15 +10,16 @@ namespace CsharpDemo
     {
         static void Main(string[] args)
         {
-            Bed bed = new Bed("Bed", 1995, 4, "Red", 130, 60, 666);
-            bed.GetInfo();
-            bed.Wash();
+            //Bed bed = new Bed("Bed", 1995, 4, "Red", 130, 60, 666);
+            //bed.GetInfo();
+            //bed.Wash();
 
             Furniture bed2 = PickFurniture.GetFurniture(MyEnum.Bed, "Good bed", 2005, 4, "Black", 150, 70, 19000);
             bed2.GetInfo();
 
             CleanTools cleanTools = new CleanTools(bed2, true, "Good Bed");
             cleanTools.Clean();
+            cleanTools.GetInfo();
         }
     }
 }
