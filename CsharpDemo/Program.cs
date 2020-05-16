e6d0a6e26f1c865b7a1f54e0e084462ecc92ae55
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-16 13:07:43 +0800

    We can Insert a new Animal and remove it.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -14,10 +14,24 @@ namespace CsharpDemo
         static void Main(string[] args)
         {
             List<Animal> animalList = new List<Animal>();
+            Animal target = new Animal("Jessica");
             animalList.Add(new Animal("Doad"));
             animalList.Add(new Animal("Jess"));
+            animalList.Add(target);
             animalList.Add(new Animal("Beca"));
             animalList.Insert(1, new Animal() { Name = "Koe" });
+            foreach (var o in animalList)
+            {
+                Console.WriteLine(o.Name);
+            }
+            Console.WriteLine("------------------------------");
+         
+            animalList.Remove(target);
+            foreach (var o in animalList)
+            {
+                Console.WriteLine(o.Name);
+            }
+            Console.WriteLine("------------------------------");
         }
     }
 }
