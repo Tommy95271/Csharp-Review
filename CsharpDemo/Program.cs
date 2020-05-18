185aff3d3687b22534ed4dc9f0d5be77101fe047
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-18 10:10:03 +0800

    We can do a small coin fliping test.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -14,25 +14,43 @@ namespace CsharpDemo
         public delegate double doubleIt(double val);
         static void Main(string[] args)
         {
-            // Lambda anonymous function, same as arrow function in js.
-            doubleIt dblIt = x => x * 2;
-            Console.WriteLine($"5 * 2 = {dblIt(5)}");
+            #region Lambda
+            //// Lambda anonymous function, same as arrow function in js.
+            //doubleIt dblIt = x => x * 2;
+            //Console.WriteLine($"5 * 2 = {dblIt(5)}");
+            #endregion
 
+            #region LINQ
             // LINQ extension
-            List<int> numList = new List<int>() { 2, 8, 7, 54, 87 };
-            var evenList = numList.Where(x => x % 2 == 0).ToList();
-            foreach (var i in evenList)
-            {
-                Console.WriteLine($"Even number is : {i}");
-            }
-            Console.WriteLine("-------------------------------------");
+            //List<int> numList = new List<int>() { 2, 8, 7, 54, 87 };
+            //var evenList = numList.Where(x => x % 2 == 0).ToList();
+            //foreach (var i in evenList)
+            //{
+            //    Console.WriteLine($"Even number is : {i}");
+            //}
+            //Console.WriteLine("-------------------------------------");
 
-            var rangeList = numList.Where(x => (x > 2) && (x < 9)).ToList();
-            foreach (var i in rangeList)
+            //var rangeList = numList.Where(x => (x > 2) && (x < 9)).ToList();
+            //foreach (var i in rangeList)
+            //{
+            //    Console.WriteLine($"Range number is : {i}");
+            //}
+            //Console.WriteLine("-------------------------------------");
+            #endregion
+
+            #region FlipCoin
+            List<int> coinList = new List<int>();
+            int i = 0;
+            Random rnd = new Random();
+            while (i < 100)
             {
-                Console.WriteLine($"Range number is : {i}");
+                coinList.Add(rnd.Next(1, 3));
+                i++;
             }
+            Console.WriteLine($"Heads is : {coinList.Where(x => x == 1).ToList().Count}");
+            Console.WriteLine($"Tails is : {coinList.Where(x => x == 2).ToList().Count}");
             Console.WriteLine("-------------------------------------");
+            #endregion
 
 
 
