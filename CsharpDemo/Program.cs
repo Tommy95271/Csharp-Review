ed5b154bee86de918e1738146ea0008b12cf8682
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-18 12:08:23 +0800

    As you can see, it is interesting to manipulate each item in two Lists.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -74,8 +74,12 @@ namespace CsharpDemo
 
             #region Zip
             List<int> list1 = new List<int>() { 1, 3, 5 };
-            List<int> list2 = new List<int>() { 7, 5, 9 };
-
+            List<int> list2 = new List<int>() { 65, 5, 9 };
+            var sumList = list1.Zip(list2, (x, y) => x + y);
+            foreach (var s in sumList)
+            {
+                Console.WriteLine($"The sum of each item in list1 and list2 is : {s}");
+            }
             #endregion
         }
     }
