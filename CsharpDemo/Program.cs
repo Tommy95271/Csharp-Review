d7b39869367248acc8f9ee424d80cb7d38b334af
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-21 22:17:48 +0800

    See? We can deal with AnimalFarm like array.

--- CsharpDemo/Program.cs
+++ CsharpDemo/Program.cs
@@ -14,8 +14,15 @@ namespace CsharpDemo
         public delegate double doubleIt(double val);
         static void Main(string[] args)
         {
-
-
+            AnimalFarm myAnimals = new AnimalFarm();
+            myAnimals[0] = new Animal("Tommy");
+            myAnimals[1] = new Animal("May");
+            myAnimals[2] = new Animal("Hami");
+            myAnimals[3] = new Animal("Luke");
+            foreach (Animal animal in myAnimals)
+            {
+                Console.WriteLine(animal.Name);
+            }
 
             Console.ReadLine();
         }
