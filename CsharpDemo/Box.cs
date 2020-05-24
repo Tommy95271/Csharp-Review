77444c5dd777d946c69775f9ad084a47487f743c
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-24 21:59:47 +0800

    Override operator + in Box, remember to use static modifier.

--- CsharpDemo/Box.cs
+++ CsharpDemo/Box.cs
@@ -8,6 +8,9 @@ namespace CsharpDemo
 {
     public class Box
     {
+        /// <summary>
+        /// Add props and constructors
+        /// </summary>
         public int Length { get; set; }
         public int Width { get; set; }
         public int Breadth { get; set; }
@@ -17,6 +20,20 @@ namespace CsharpDemo
             Length = l;
             Width = w;
             Breadth = b;
+        }
+        /// <summary>
+        /// Override operator + in Box, remember to use static modifier.
+        /// </summary>
+        /// <param name="box1"></param>
+        /// <param name="box2"></param>
+        /// <returns></returns>
+        public static Box operator +(Box box1, Box box2)
+        {
+            Box newBox = new Box();
+            newBox.Length = box1.Length + box2.Length;
+            newBox.Width = box1.Width + box2.Width;
+            newBox.Breadth = box1.Breadth + box2.Breadth;
+            return newBox;
         }
     }
 }
