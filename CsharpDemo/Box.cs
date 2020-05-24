e9ffe0661b39bb1f952b594ead7f91e7e3622e70
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-24 21:55:55 +0800

    Add props and constructors.

--- CsharpDemo/Box.cs
+++ CsharpDemo/Box.cs
@@ -8,6 +8,15 @@ namespace CsharpDemo
 {
     public class Box
     {
-
+        public int Length { get; set; }
+        public int Width { get; set; }
+        public int Breadth { get; set; }
+        public Box() : this(1, 1, 1) { }
+        public Box(int l, int w, int b)
+        {
+            Length = l;
+            Width = w;
+            Breadth = b;
+        }
     }
 }
